4b7e33e64c65e8fe7d36971a3ccdbe4710d318d8
Kevin Davis <thekidnamedkd@gmail.com>
2020-06-16 13:58:37 -0700

    add radio button options for selection input. add business logic for ticket cost.
    Co-authored-by: Spencer Moody <spencer.moody@outlook.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,42 @@
+// Business Logic -----
+function Ticket (movieTitle, movieTime, userAge) {
+  this.movieTitle = movieTitle;
+  this.movieTime = movieTime;
+  this.userAge = userAge;
+}
+
+// Calculate ticket cost -----
+Ticket.prototype.ticketPrice = function() {
+  //
+  this.price = 10;
+  if (this.movieTitle === "new") {
+    this.price += 3
+  } else if (this.movieTitle === "old") {
+    this.price += 1
+  }
+  if (this.userAge === "adult") {
+    this.price += 2
+  } else if (this.userAge === "senior") {
+    this.price += 1
+  } else {
+    this.price
+  }
+  if (this.movieTime === "matinee") {
+    this.price += 1
+  } else if (this.movieTime === "evening") {
+    this.price += 2
+  } else {
+    this.price
+  }
+  return this.price
+}
+
+// User Interface -----
 $(document).ready(function() {
-event.preventDefault();
+  $("form#ticketMatrix").submit(function(event) {
+    event.preventDefault();
+    let movieTitle = $("input:radio[name=title]:checked").val();
+    let movieTime = $("input:radio[name=time]:checked").val();
+    let userAge = $("input:radio[name=time]:checked").val();
+  })  
 })
